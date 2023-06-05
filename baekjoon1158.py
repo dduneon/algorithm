dbecfbc7ca3d234775f89958ca6b75c022d95ac6
xpexpe <xpexpe3@icloud.com>
2023-06-05 15:22:51 +0900

    백준 1158: 요세푸스 문제

--- baekjoon1158.py
+++ baekjoon1158.py
@@ -1,3 +1,26 @@
 import sys
+
+
+class Node:
+
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
 N, K = sys.stdin.readline().strip().split()
+start = Node(0)
+head = Node(1)
+start.next = head
+cur = head
+for i in range(2, int(N)+2):
+    newnode = Node(i)
+    cur.next = newnode
+    cur = newnode
+
+cur.next = head
+#
+while(head.next):
+    print(head.data)
+    head = head.next
 
