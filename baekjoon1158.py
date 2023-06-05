c34356810b4b21b88bd814467c22a8f3613beb3d
dduneon <dduneon@gmail.com>
2023-06-05 17:44:01 +0900

    백준 1158: 요세푸스 문제

--- baekjoon1158.py
+++ baekjoon1158.py
@@ -13,14 +13,22 @@ start = Node(0)
 head = Node(1)
 start.next = head
 cur = head
-for i in range(2, int(N)+2):
+for i in range(2, int(N)+1):
     newnode = Node(i)
     cur.next = newnode
     cur = newnode
-
 cur.next = head
-#
-while(head.next):
-    print(head.data)
-    head = head.next
+# 원형 LinkedList 구축
 
+print('<', end='')
+if int(K) != 1:
+    while start != start.next:
+        for _ in range(int(K)-1):
+            start = start.next
+        print(start.next.data, end=', ')
+        start.next = start.next.next
+    print(start.data, end='>')
+else:
+    for i in range(1, int(N)):
+        print(i, end=', ')
+    print(int(N), end='>')
---
+++ baekjoon1158_1.py
@@ -0,0 +1,7 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+
+L = [n+1 for n in range(N)]
+
+
