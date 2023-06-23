05f255cd38fde0782b91d7af83f3e3cc522cec6a
dduneon <dduneon@gmail.com>
2023-06-24 03:38:16 +0900

    백준 15686번: 치킨 배달

--- baekjoon15686.py
+++ baekjoon15686.py
@@ -14,14 +14,18 @@ for i in range(N):
         elif row[j] == 2:
             chicken.append((i, j))
 
-selected = list(combinations(chicken, M))
-for r1, c1 in selected:
-    tmp_distance = 0
-    for r2, c2 in house:
-        tmp_distance += distance(r1, c1, r2, c2)
+answer = []
+
 def distance(r1, c1, r2, c2):
     return abs(r1-r2) + abs(c1-c2)
 
-
-
-
+for chicken_combination in combinations(chicken, M):
+    ck_distance = 0
+    for r2, c2 in house:
+        tmp_distance = float('inf')
+        for i in range(M):
+            r1, c1 = chicken_combination[i]
+            tmp_distance = min(tmp_distance, distance(r1, c1, r2, c2))
+        ck_distance += tmp_distance
+    answer.append(ck_distance)
+print(min(answer))
