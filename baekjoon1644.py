167196a285776db74adc6f20166ab1595ad47f15
dduneon <dduneon@gmail.com>
2023-06-30 03:58:41 +0900

    백준 1644번: 소수의 연속합(풀이1)

--- baekjoon1644.py
+++ baekjoon1644.py
@@ -1,7 +1,8 @@
 import sys
 N = int(sys.stdin.readline())
 
-prime = [True for _ in range(4000001)]
+prime = [True for _ in range(N+1)]
+prime[0], prime[1] = False, False
 
 for i in range(2, int(N**0.5) + 1):
     if prime[i]:
@@ -10,4 +11,17 @@ for i in range(2, int(N**0.5) + 1):
             prime[i*j] = False
             j+=1
 
-print(prime[1], prime[2], prime[3], prime[4], prime[5], prime[6])
+primes = [i for i in range(2, N+1) if prime[i]]
+result = 0
+
+for p in range(len(primes)):
+    tmp = 0
+    for a in range(p, len(primes)):
+        tmp += primes[a]
+        if tmp == N:
+            result += 1
+            break
+        elif tmp > N:
+            break
+
+print(result)
