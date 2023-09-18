a68b89f58a4270655053bdc4a13f0986f760952b
dduneon <dduneon@gmail.com>
2023-09-18 16:58:52 +0900

    1260

--- Main.java
+++ Main.java
@@ -1,8 +1,22 @@
-import java.util.LinkedList;
+
+// 1260: DFS, BFS
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<Integer> link = new LinkedList<>();
+        int N, M, V;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        
+        boolean[] isVisited = new boolean[]
+
         
     }
 }
