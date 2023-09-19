b567969e88840d27136a16a507afe9ef67b91ab4
JUNHYEON <84072084+dduneon@users.noreply.github.com>
2023-09-19 17:43:40 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,35 @@
-
-// 1260: DFS, BFS
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        int N, M, V;
+    static int[][] map;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sizeOfMap = Integer.parseInt(br.readLine());
+        map = new int[sizeOfMap][sizeOfMap];
+        visited = new boolean[sizeOfMap][sizeOfMap];
+        StringTokenizer st;
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        V = Integer.parseInt(st.nextToken());
-        
-        boolean[] isVisited = new boolean[]
+        for (int i = 0; i < sizeOfMap; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < sizeOfMap; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-        
+        for (int i = 0; i < sizeOfMap; i++) {
+            for (int j = 0; j < sizeOfMap; j++) {
+                if (!visited[i][j])
+                    dfs(i, j);
+            }
+        }
+    }
+
+    static void dfs(int row, int col) {
+
     }
 }
