f7d1075d5bd327e0bee0a635a4969c39211cb55e
JUNHYEON <84072084+dduneon@users.noreply.github.com>
2023-10-25 15:03:25 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 18120 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -5,42 +5,18 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(br.readLine());
-        boolean[] visited = new boolean[number + 1];
-        int result = BFS(number, visited);
-        System.out.println(result);
-    }
+        int[] dp = new int[number + 1];
 
-    public static int BFS(int start, boolean[] visited) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(start, 0));
-        visited[start] = true;
-
-        while (!queue.isEmpty()) {
-            Node popItem = queue.poll();
-            int value = popItem.value;
-            int depth = popItem.depth;
-
-            if (value == 1)
-                return depth;
-
-            if (value % 3 == 0 && !visited[value / 3])
-                queue.add(new Node(value / 3, depth + 1));
-            if (value % 2 == 0 && !visited[value / 2])
-                queue.add(new Node(value / 2, depth + 1));
-            if (value > 1 && !visited[value - 1])
-                queue.add(new Node(value - 1, depth + 1));
+        for (int i = 2; i <= number; i++) {
+            // default case
+            dp[i] = dp[i - 1] + 1;
+            // divide with 3
+            if (i % 3 == 0)
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            // divide with 2
+            if (i % 2 == 0)
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
         }
-        return -1;
-    }
-}
-
-
-class Node {
-    int value;
-    int depth;
-
-    public Node(int value, int depth) {
-        this.value = value;
-        this.depth = depth;
+        System.out.println(dp[number]);
     }
 }
