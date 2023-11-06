f550b7720f9b13d5443bb81917f91552ff3f58a0
JUNHYEON <84072084+dduneon@users.noreply.github.com>
2023-11-06 10:45:24 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 579 ms, Memory: 151,216 KB -BaekjoonHub

---
+++ SWEA/D2/1859. 백만 장자 프로젝트/README.md
@@ -0,0 +1,15 @@
+# [D2] 백만 장자 프로젝트 - 1859 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LrsUaDxcDFAXc) 
+
+### 성능 요약
+
+메모리: 151,216 KB, 시간: 579 ms, 코드길이: 1,141 Bytes
+
+### 제출 일자
+
+2023-11-06 10:45
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -0,0 +1,35 @@
+//package solving;
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int N = Integer.parseInt(br.readLine());
+            List<Integer> array = new ArrayList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) {
+                array.add(Integer.parseInt(st.nextToken()));
+            }
+
+            System.out.println("#" + test_case + " " + solution(N, array));
+        }
+    }
+
+    public static long solution(int N, List<Integer> array) {
+        long result = 0;
+        int max_sell = array.get(N - 1);
+        for (int i = N - 2; i >= 0; i--) {
+            int indexedItem = array.get(i);
+            if (indexedItem >= max_sell) {
+                max_sell = indexedItem;
+            } else {
+                result += max_sell - indexedItem;
+            }
+        }
+        return result;
+    }
+}
