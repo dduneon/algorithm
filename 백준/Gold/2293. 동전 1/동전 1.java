6b788331e9e4ba1c98e8c0afb2c7d16cb9b32c76
JUNHYEON <84072084+dduneon@users.noreply.github.com>
2023-12-22 15:15:45 +0900

    [Gold V] Title: 동전 1, Time: 132 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,30 @@
+//package solving;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] input = br.readLine().split(" ");
+    int N = Integer.parseInt(input[0]);
+    int K = Integer.parseInt(input[1]);
+
+    int[] numbers = new int[N];
+    for (int i = 0; i < N; i++) {
+      numbers[i] = Integer.parseInt(br.readLine());
+    }
+
+    int[] dp = new int[K + 1];
+    dp[0] = 1;
+
+    for (int num : numbers) {
+      for (int i = num; i <= K; i++) {
+        dp[i] = dp[i] + dp[i - num];
+      }
+    }
+    System.out.println(dp[K]);
+  }
+}
