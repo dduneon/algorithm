8d690cfeee2a148e05746a6d39ea9ab30995126a
JUNHYEON <dduneon.dev@gmail.com>
2024-04-02 10:43:55 +0900

    [Silver IV] Title: 로프, Time: 336 ms, Memory: 26828 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] weights = new int[N];
+        for(int i=0; i<N; i++) {
+            weights[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(weights);
+
+        // 들수 있는 최대 중량은 로프 한개가 들 수 있는 최소 중량 * n(사용된 로프의 수)
+        int maxWeight = 0;
+        for(int i=0; i<N; i++) {
+            maxWeight = Math.max(maxWeight, weights[i] * (N-i));
+        }
+
+        System.out.println(maxWeight);
+    }
+}
