232d027862b051e6de906260359379563e72b0bb
JUNHYEON <dduneon.dev@gmail.com>
2024-04-16 15:00:22 +0900

    [Silver III] Title: 주유소, Time: 580 ms, Memory: 43516 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -2,21 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine()) - 1;
-        StringTokenizer distanceTokenizer = new StringTokenizer(br.readLine());
-        StringTokenizer priceTokenizer = new StringTokenizer(br.readLine());
-
-        int[] distance = new int[N];
-        int[] price = new int[N];
-        for(int i=0; i<N; i++) {
-            distance[i] = Integer.parseInt(distanceTokenizer.nextToken());
-            price[i] = Integer.parseInt(priceTokenizer.nextToken());
-        }
+        int[] distance = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] price = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         int[][] station = new int[N][2];
         int remain = 0;
@@ -28,13 +20,13 @@ public class Main {
 
         Arrays.sort(station, (o1, o2) -> o1[1]-o2[1]);
 
-        int result = 0;
+        long result = 0;
         int current = 0;
         for(int[] st: station) {
             if(st[0] > current) {
                 int buy = st[0] - current;
-                result = result + buy * st[1];
-                current += st[0];
+                result = result + (long) buy * st[1];
+                current += buy;
             }
         }
         System.out.println(result);
