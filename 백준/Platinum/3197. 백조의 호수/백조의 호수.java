30156e68bff3541cbd053d94e06bef5b6b2d8bfd
JUNHYEON <dduneon.dev@gmail.com>
2024-04-21 17:39:02 +0900

    [Platinum V] Title: 백조의 호수, Time: 804 ms, Memory: 206752 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.java
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.java
@@ -14,7 +14,6 @@ public class Main {
     static Queue<Pos> watertmp = new LinkedList<>();
     static Queue<Pos> swans = new LinkedList<>();
     static Queue<Pos> swanstmp = new LinkedList<>();
-    static int[] swansFirstPos = null;
 
     public static int R, C;
 
@@ -45,6 +44,7 @@ public class Main {
         map = new char[R][C];
         visited = new boolean[R][C];
 
+        boolean isFirst = false;
         // 맵을 입력받고, 물과 백조의 위치를 큐에 추가
         for(int i = 0; i < R; i++) {
             String line = br.readLine();
@@ -54,11 +54,13 @@ public class Main {
                 if(map[i][j] == '.') {
                     water.add(new Pos(i, j));
                 } else if(map[i][j] == 'L') {
-                    if(swansFirstPos == null) {
+                    // 백조는 한마리를 기준으로 진행하며 처음 위치를 기록
+                    if(!isFirst) {
                         swans.add(new Pos(i, j));
-                        swansFirstPos = new int[]{i, j};
                         visited[i][j] = true;
+                        isFirst = true;
                     }
+                    // 백조가 있는 위치 또한 물이므로, 추가
                     water.add(new Pos(i, j));
                 }
             }
@@ -74,12 +76,14 @@ public class Main {
                 int col = current.col + move[1];
 
                 if(Pos.isValidPos(row, col) && !visited[row][col]) {
+                    // 방문하지 않은 위치를 방문, 물인 경우 오늘 하루동안 계속 진행할 수 있고, 얼음인 경우 내일까지 기다려야 하므로 다른 Queue에 삽입
                     visited[row][col] = true;
                     if(map[row][col] == '.') {
                         swans.add(new Pos(row, col));
                     } else if(map[row][col] == 'X') {
                         swanstmp.add(new Pos(row, col));
-                    } else if(map[row][col] == 'L' && (swansFirstPos[0] != row || swansFirstPos[1] != col)) {
+                    } else if(map[row][col] == 'L') {
+                        // 백조의 위치에 도착했다면 성공, true 반환
                         return true;
                     }
                 }
@@ -95,6 +99,9 @@ public class Main {
                 int row = current.row + move[0];
                 int col = current.col + move[1];
 
+                // 주변 얼음들을 찾으며, 얼음들을 큐에 삽입함
+                // 만약 그냥 물이라면, 파악할 필요가 없고 방문 여부를 기록해두지 않는 이유는 방문 여부가 없어도 얼음인지 물인지를 판단하는 과정을 통해
+                // 중복된 삽입을 하지 않을 수 있기 때문이다
                 if(Pos.isValidPos(row, col) && map[row][col] == 'X') {
                     watertmp.add(new Pos(row, col));
                     map[row][col] = '.';
