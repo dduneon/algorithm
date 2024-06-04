cd2930c9b7592a7144dcd860e24db27c5667c8d8
JUNHYEON <dduneon.dev@gmail.com>
2024-06-04 13:44:06 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
