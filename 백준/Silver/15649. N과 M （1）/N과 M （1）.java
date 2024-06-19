1f4eda13b6563d3bda38aa809aa55172e47ee161
JUNHYEON <dduneon.dev@gmail.com>
2024-06-19 17:01:01 +0900

    [Silver III] Title: N과 M (1), Time: 252 ms, Memory: 22100 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int N;
+    private static int M;
+    private static boolean[] visited;
+    private static int[] result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        result = new int[M];
+
+        sb = new StringBuilder();
+        // 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+        dfs(1, 1, 0);
+        System.out.print(sb);
+    }
+
+    private static StringBuilder sb;
+
+    private static void dfs(int current, int past, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result[depth] = i;
+                dfs(i, current, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
